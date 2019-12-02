031d0442478de0ffd82f1b66ac5a5bdaa6714ed1
Tomasz Jonak <tcjonak@gmail.com>
2019-12-03 00:12:49 +0100

    Day1 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let stdlocked = stdin.lock();
+
+    let result: u64 = stdlocked
+        .lines()
+        .filter_map(|x| x.ok())
+        .filter_map(|x| x.parse::<u64>().ok())
+        .map(|x| x / 3 - 2)
+        .sum();
+    println!("{}", result);
 }
